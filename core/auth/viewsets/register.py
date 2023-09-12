8981e968593555b87ba1ecb7e73a8927df769281
Joey <hanazzang999@gmail.com>
2023-09-12 10:41:06 +0900

    register router

--- core/auth/viewsets/register.py
+++ core/auth/viewsets/register.py
@@ -12,15 +12,18 @@ class RegisterViewSet(ViewSet):
     permission_classes = (AllowAny,)
     http_method_names = ['post']
 
+    #실제로 회원가입을 처리
     def create(self,request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        #유효성 검사 통과하면 사용자를 생성하고, 해당 사용자에 대한 JWT refresh 토근을 생성함
         refresh = RefreshToken.for_user(user)
         res = {
             'refresh' : str(refresh),
             'access': str(refresh.access_token),
         }
+
         return Response({
             'user':serializer.data,
             'refresh':res['refresh'],
