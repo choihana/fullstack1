2d6da93a38e58c7a95f95e0fcb0e93712c2cd105
Joey <hanazzang999@gmail.com>
2023-09-12 11:34:19 +0900

    login router

--- core/auth/viewsets/login.py
+++ core/auth/viewsets/login.py
@@ -1,8 +1,9 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from rest_framework.permissions import AllowAny
-from rest_framework import status
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+
 from core.auth.serializers import LoginSerializer
 
 
@@ -13,10 +14,8 @@ class LoginViewSet(ViewSet):
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
-
         try:
             serializer.is_valid(raise_exception=True)
         except TokenError as e:
             raise InvalidToken(e.args[0])
-
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
--- core/auth/viewsets/register.py
+++ core/auth/viewsets/register.py
@@ -3,8 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-
-from core.auth.serializers.register import RegisterSerializer
+from core.auth.serializers import RegisterSerializer
 
 
 class RegisterViewSet(ViewSet):
