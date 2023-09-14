d99ce303a79c9e1c230863358cdf3cbcdd34d397
Joey <hanazzang999@gmail.com>
2023-09-14 13:48:08 +0900

    [test] user create

--- CoreRoot/tests.py
+++ CoreRoot/tests.py
@@ -1,5 +1,5 @@
-def add(a,b):
-    return a+b
-def test_sum():
-    assert add(1,2) == 3
-    assert add(2,2) == 3
+# def add(a,b):
+#     return a+b
+# def test_sum():
+#     assert add(1,1) == 3
+
--- core/user/tests.py
+++ core/user/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+import pytest
+from core.user.models import User
+
+data_user ={
+    "username": "test_user",
+    "email": "test@gmail.com",
+    "first_name": "TEST",
+    "last_name": "USER",
+    'password':"test_password"
+}
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create_user(**data_user)
+    assert user.username == data_user["username"]
+    assert user.email == data_user["email"]
+    assert user.first_name == data_user["first_name"]
+    assert user.last_name == data_user["last_name"]
